9a94cb35b02942d838ecb91f027819483d5303e0
diana galuh <velfors@gmail.com>
2020-12-03 18:13:51 +0300

    написали методф класса

--- soccerPlayer.cpp
+++ soccerPlayer.cpp
@@ -1,20 +1,34 @@
-﻿// soccerPlayer.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include"SoccerPlayer.h"
+SoccerPlayer::SoccerPlayer() {
+	name = "";
+	surname = "";
+	goals = 0;
+	goals_Assists = 0;
 
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
 }
+SoccerPlayer::SoccerPlayer(std::string fName, std::string fSurName, unsigned int fGoals, unsigned int fgoals_Assists) {
+	name = "fName";
+	surname = "fSurName";
+	goals = fGoals;
+	goals_Assists = fgoals_Assists;
+}
+	void SoccerPlayer::setName(std::string fName){ name = "fName"; }
+	void SoccerPlayer::setSurName(std::string fSurName){ surname = "fSurName"; }
+	void SoccerPlayer::setGoals(unsigned int fGoals){ goals = fGoals; }
+	void SoccerPlayer::setAssists(unsigned int fgoals_Assists){ goals_Assists = fgoals_Assists; }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+	std::string SoccerPlayer::getName() { return name; };
+	std::string SoccerPlayer::getSurName() { return surname; };
+	unsigned int SoccerPlayer::getGoals() { return goals; };
+	unsigned int SoccerPlayer::getAssists() { return goals_Assists; };
+
+	void SoccerPlayer::print() {
+		std::cout << surname << ' ' << name << " goals: " << goals << " assists: " << goals_Assists << std::endl;
+	}
+	void SoccerPlayer::addGoals(unsigned int fg){
+		goals += fg;
+	}
+	void SoccerPlayer::addAssists(unsigned int fa){
+		goals_Assists += fa;
+	}
